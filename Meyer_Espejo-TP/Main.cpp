736cefe73a798b8c765b3007deba5f8df0178c3b
matthewmirror1 <matuespejo@gmail.com>
2023-04-29 11:29:45 -0300

    main

---
+++ Meyer_Espejo-TP/Main.cpp
@@ -0,0 +1,22 @@
+#include "Libreria.h"
+#include "Articulos.h"
+#include <iostream>
+#include "CarritoCompras.h"
+#define n=3
+
+
+
+int main() {
+	vector<Articulos> ListaArticulos;
+	vector<CarritoCompras>ListaPedido;
+	
+
+
+
+
+
+
+
+
+	return(0);
+}
