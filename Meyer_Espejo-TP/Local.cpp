cb8b08690e183ed4d407a8751b00da028e4049fa
matthewmirror1 <matuespejo@gmail.com>
2023-05-17 17:31:13 -0300

    cambio_local

--- Meyer_Espejo-TP/Local.cpp
+++ Meyer_Espejo-TP/Local.cpp
@@ -1,11 +1,17 @@
 
 #include "Local.h"
-
-Local::Local(string Nombre, string Direccion, string Contacto) {
+#include <queue>
+Local::Local(string Nombre, string Direccion, string Contacto, queue<Cliente> cola_Clientes) {
 	this->Nombre = Nombre;
 	this->Direccion = Direccion;
 	this->Contacto = Contacto;
+	for (int i = 0; i < cola_Clientes.size(); i++)
+	{
+		this-> cola_Clientes.push();
+	}
 }
+
+
 Local::~Local() {}
 
 
--- Meyer_Espejo-TP/Local.h
+++ Meyer_Espejo-TP/Local.h
@@ -2,10 +2,14 @@
 #ifndef _LOCAL_H
 #define _LOCAL_H
 #include "Header.h"
+#include <queue>
+#include "Cliente.h"
+
+
 
 class Local {
 public:
-    Local(string Nombre, string Direccion, string Contacto);
+    Local(string Nombre, string Direccion, string Contacto, queue<Cliente> cola_Clientes);
     ~Local();
   
 
@@ -13,6 +17,8 @@ private:
     string Nombre;
     string Direccion;
     string Contacto;
+    queue<Cliente> cola_Clientes;
+
 };
 
 #endif //_LOCAL_H
