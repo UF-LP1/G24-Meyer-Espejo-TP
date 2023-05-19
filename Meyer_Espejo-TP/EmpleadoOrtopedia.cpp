904f0c39c51c4cd16ba41e9a0bd4017449bda43b
matthewmirror1 <matuespejo@gmail.com>
2023-05-19 11:50:17 -0300

    cambio_perf

--- Meyer_Espejo-TP/EmpleadoOrtopedia.cpp
+++ Meyer_Espejo-TP/EmpleadoOrtopedia.cpp
@@ -2,7 +2,7 @@
 
 
 #include "EmpleadoOrtopedia.h"
-
+#include "eArticulosLocal.h"
 
 EmpleadoOrtopedia::EmpleadoOrtopedia(string nombre, string apellido, float sueldo) :Vendedor(nombre,apellido,sueldo) {
 
@@ -13,19 +13,19 @@ EmpleadoOrtopedia::~EmpleadoOrtopedia(){}
 
 
 
-//void EmpleadoOrtopedia:: revisionort(Cliente MiCliente) {
-//    vector<Articulos>aux;
-//    aux = MiCliente.get_ProductosQuiero();
-//    for (int i = 0; i < aux.size(); i++)
-//    {
-//        if (aux[i].get_Stock()>1) {
-//            //aca agregar al carrito
-//        }
-//
-//    }
-//
-//
-//
-//}
+void EmpleadoOrtopedia:: revisionort(Cliente MiCliente) {
+    vector<eArticulosLocal>aux;
+    aux = MiCliente.get_ProductosQuiero();
+    for (int i = 0; i < aux.size(); i++)
+    {
+        if (aux[i] > 10 && aux[i] <15 ) {
+            //aca agregar al carrito
+        }
+
+    }
+
+
+
+}
 
 
--- Meyer_Espejo-TP/EmpleadoPerfumeria.cpp
+++ Meyer_Espejo-TP/EmpleadoPerfumeria.cpp
@@ -9,20 +9,18 @@ EmpleadoPerfumeria::~EmpleadoPerfumeria() {}
 
 
 bool EmpleadoPerfumeria:: revisionperf(Cliente MiCliente) {
-    vector<string>aux;
+    vector<eArticulosLocal>aux;
     aux = MiCliente.get_ProductosQuiero();
     for (int i = 0; i < aux.size(); i++)
     {
-        if (aux[i] == e)
-        {
-            if (aux[i].get_Stock() > 1) {
-                //aca agregar al carrito
-                return true;
-           
-                //Perfumeria ArticuloAagregar(aux[i].get_Precio(), aux[i].get_Stock(),aux[i].get_Codigo(),aux[i].get_);
-            }
+        if (aux[i] > 2 && aux[i]<11) {
+            //aca agregar al carrito
         }
+
     }
+
+
+
 }
 
 
